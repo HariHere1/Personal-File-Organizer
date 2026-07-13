3dd6b263c234abc128a1f72f2e57017966d191c4
HariHere1 <hg6674763@gmail.com>
2026-07-13 22:46:09 +0530

    feat: accept folder path from user

---
+++ main.py
@@ -0,0 +1,5 @@
+# Task 1 : Ask the user to enter the path of the folder they want to organize and store it in a variable.
+
+path = input("Enter the path of the folder you want to organize & store")
+
+
