5b0bac9786545ce9d904eae6cd913e2c653e14a0
HariHere1 <hg6674763@gmail.com>
2026-07-13 23:09:58 +0530

    feat: Lists directory contents

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ path = input("Enter the path of the folder you want to organize & store : ")
 
 if os.path.isdir(path)== True:
     print("Valid path")
+    print(os.listdir(path))
+
 
 else:
     print("Invalid path")
+
+
