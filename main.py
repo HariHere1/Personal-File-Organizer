2cfa63aef2408cc0d09f0639d1307ac9c6151c79
HariHere1 <hg6674763@gmail.com>
2026-07-18 06:34:46 +0530

    Add gitignore

--- main.py
+++ main.py
@@ -2,12 +2,22 @@ import os
 
 path = input("Enter the path of the folder you want to organize & store : ")
 
-if os.path.isdir(path)== True:
-    print("Valid path")
-    print(os.listdir(path))
+dir_elements = []
 
+if os.path.isdir(path) == True:
+    print("\nValid path\n")
+    dir_elements = os.listdir(path)
 
 else:
     print("Invalid path")
+
+for element in dir_elements:
+    full_path = os.path.join(path, element)
+
+    if os.path.isfile(full_path):
+        print(element,' is a file \n')
+    else:
+        print(elementsSo ,' is not a file')
+
 
 
